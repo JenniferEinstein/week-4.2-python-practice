d0f300567c227ff7c86b0f6a626200b40339ad12
JenniferEinstein <jennifereinstein@gmail.com>
2024-10-16 15:27:59 -0400

    Completed bulk of functions. Added global variables.

--- lib/script.py
+++ lib/script.py
@@ -1,4 +1,6 @@
+import random
 #from capitals import states
+#print(states)
 
 # an array of sample of state dictionaries
 states = [
@@ -27,17 +29,57 @@ states = [
     "name": "Montana",
     "capital": "Helena"
 }]
-print(states)
 
+
+# Randomize the given state list
+def shuffle_states():
+    random.shuffle(states)
+    return states
+
+shuffled_states = shuffle_states()
 
 # Request for user input
 name = input("Enter your name: ")
 continuation = input("Do you want to continue? (Yes/No): ")
 
+# ======  SCORE TRACKING VARIABLES ====== #
+correct_answers = 0
+incorrect_answers = 0
 
-state = "state"
-question = "What is the capital of" + state + "?"
-student_answer = input(question)
 
-if student_answer.upper() == 
 
+def ask_questions(shuffled_states):
+    global correct_answers, incorrect_answers  ## allows modification of variable outside of the current scope
+
+    for state in shuffled_states:
+        question = f"What is the capital of {state['name']}?"
+        student_answer = input(question)
+        check_answer(student_answer, state)
+
+def check_answer(student_answer, state):
+    global correct_answers, incorrect_answers
+    if student_answer.strip().lower() == state['capital'].strip().lower():
+        correct_answers += 1
+        print(f"Correct! The capital of {state['name']} is {state['capital']}.")
+    else:
+        incorrect_answers += 1
+        print(f"I'm sorry. The capital of {state['name']} is {state['capital']}.")
+
+    def display_score():
+        if correct_answers > 0 and incorrect_answers > 0:
+            print("So far, you correctly  identified " + correct_answers + "state capitals and incorrectly identified " + incorrect_answers + " state capitals.")
+        elif correct_answers > 0 and incorrect_answers == 0:
+            print("So far, you have correctly identified " + correct_answers + " state capitals.")
+        elif correct_answers == 0 and incorrect_answers > 0:
+            print("So far, you have incorrectly identified " + incorrect_answers + " state capitals.")
+
+
+
+# =========  FUNCTIONS  ============
+#  DONE  shuffle_states(): Randomize the order of states
+# display_question(state): Show the current state to the user
+# get_user_input(): Prompt and receive user's guess
+#  DONE  check_answer(user_input, correct_capital): Verify the user's answer
+# update_score(is_correct): Modify the user's score
+#  DONE  display_score(): Show current or final score
+# play_again(): Ask if the user wants to restart the game
