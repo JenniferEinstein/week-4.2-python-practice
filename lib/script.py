220130351cd5999fb10e06feebd45ef8fbe4c7f5
JenniferEinstein <jennifereinstein@gmail.com>
2024-10-16 14:46:40 -0400

    First commit. Starting with input questions.

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,43 @@
-from capitals import states
+#from capitals import states
 
+# an array of sample of state dictionaries
+states = [
+ {
+    "name": "Maine",
+    "capital": "Augusta"
+}, {
+    "name": "Maryland",
+    "capital": "Annapolis"
+}, {
+    "name": "Massachusetts",
+    "capital": "Boston"
+}, {
+    "name": "Michigan",
+    "capital": "Lansing"
+}, {
+    "name": "Minnesota",
+    "capital": "St. Paul"
+}, {
+    "name": "Mississippi",
+    "capital": "Jackson"
+}, {
+    "name": "Missouri",
+    "capital": "Jefferson City"
+}, {
+    "name": "Montana",
+    "capital": "Helena"
+}]
 print(states)
+
+
+# Request for user input
+name = input("Enter your name: ")
+continuation = input("Do you want to continue? (Yes/No): ")
+
+
+state = "state"
+question = "What is the capital of" + state + "?"
+student_answer = input(question)
+
+if student_answer.upper() == 
+
